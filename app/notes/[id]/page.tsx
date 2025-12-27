e1ee93fba141d46f3559f8f73ccce8ee9e797b5f
Vasyl Hrubyi <120045095+VasylHru@users.noreply.github.com>
2025-12-27 12:23:44 +0200

    Initial commit

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,54 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import type { Metadata } from "next";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client";
+
+type PageProp = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: PageProp): Promise<Metadata> {
+  const { id } = await params;
+
+  const note = await fetchNoteById(id);
+  return {
+    title: note.title,
+    description: note.content,
+    openGraph: {
+      title: note.title,
+      description: note.content,
+      url: `/notes/${id}`,
+      images: [
+        {
+          url: `/note/${id}`,
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+    },
+  };
+}
+
+export default async function NoteDetailsPage({ params }: PageProp) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id} />
+    </HydrationBoundary>
+  );
+}
---
+++ app/notes/filter/[...slug]/page.tsx
@@ -0,0 +1,67 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import type { Metadata } from "next";
+import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: Promise<{ slug: string[] }>;
+};
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const url = slug.join('/');
+  const tagName = slug[slug.length - 1];
+
+  return {
+    title: ` ${tagName}`,
+    description: `Page with selected filters: ${url}`,
+    openGraph: {
+      title: `Notes: ${tagName}`,
+      description: `Page with selected filters: ${url}`,
+      url: `https://notehub.com/notes/filter/${url}`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: `Notes: ${tagName}`,
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
+
+export default async function NotesFilterPage({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
+}) {
+  const queryClient = new QueryClient();
+
+  const { slug } = await params;
+
+  const VALID_TAGS = ["all", "Todo", "Work", "Personal", "Meeting", "Shopping"];
+  const tagParam = slug?.[0];
+  const normalizedTag = tagParam === "all" ? undefined : tagParam;
+
+  if (!VALID_TAGS.includes(tagParam)) {
+    notFound();
+  }
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", normalizedTag, 1, ""],
+    queryFn: () => fetchNotes(1, normalizedTag, ""),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient tag={normalizedTag} />
+    </HydrationBoundary>
+  );
+}
